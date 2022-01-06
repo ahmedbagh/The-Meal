e3a71d74d4d26351da1f079df2e9cb407fcdacf5
Ahmed BAGHAWITAH <ahmedbagh1997@gmail.com>
2022-01-06 16:23:28 +0100

    Add the recyclerView & adapter for the categories

--- app/src/main/java/com/example/the_meal/ui/viewmodel/uistate/CategoriesUiState.kt
+++ app/src/main/java/com/example/the_meal/ui/viewmodel/uistate/CategoriesUiState.kt
@@ -1,9 +1,9 @@
-package com.example.the_meal.viewmodel.uistate
+package com.example.the_meal.ui.viewmodel.uistate
 
 import com.example.the_meal.data.model.CategoryApiModel
 
 data class CategoriesUiState(
-    val isLoading : Boolean = false,
+    val isLoading : Boolean = true,
     val isErr : Boolean = false,
     var errMessage : String = "",
     var categories : List<CategoryApiModel> = listOf()
--- app/src/main/java/com/example/the_meal/ui/viewmodel/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/example/the_meal/ui/viewmodel/viewmodel/CategoriesViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.the_meal.viewmodel
+package com.example.the_meal.ui.viewmodel.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.the_meal.data.repository.CategoriesRepository
-import com.example.the_meal.viewmodel.uistate.CategoriesUiState
+import com.example.the_meal.ui.viewmodel.uistate.CategoriesUiState
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import java.io.IOException
@@ -22,12 +22,11 @@ class CategoriesViewModel(
     }
 
     fun fetchCategories() {
-        // TODO : Update the uiState
         fetchJob?.cancel()
         fetchJob = viewModelScope.launch {
             try {
                 val categories = categoriesRepository.fetchListOfAllCategories()
-                uiState.value = CategoriesUiState(categories = categories)
+                uiState.value = CategoriesUiState(categories = categories, isLoading = false)
             } catch (ioe: IOException) {
                 val messages = ioe.message!!
                 uiState.value = CategoriesUiState(isErr = true, errMessage = messages)
